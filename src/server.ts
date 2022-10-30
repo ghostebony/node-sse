010489a82d358beec4ef5f7b3c2b876fa9744aa6
ghostebony <pedroamericolouzada@uol.com.br>
2022-10-30 14:11:40 -0300

    add server

---
+++ src/server.ts
@@ -0,0 +1,119 @@
+import type { Channel, Controller, MessageData, MessageId, OnAction, Room, User } from "./types";
+
+export class Server {
+	public users = new Map<User, Set<Controller>>();
+
+	public constructor(public room: Room) {}
+
+	public server = (
+		user: User,
+		on?: {
+			connect?: OnAction;
+			disconnect?: OnAction;
+		}
+	) =>
+		new Response(new ReadableStream(this.controller(user, on)), {
+			headers: {
+				connection: "keep-alive",
+				"cache-control": "no-store",
+				"content-type": "text/event-stream",
+			},
+		});
+
+	private controller = (
+		user: User,
+		on?: {
+			connect?: OnAction;
+			disconnect?: OnAction;
+		}
+	): UnderlyingSource<unknown> => {
+		let controller: Controller;
+		let ping: ReturnType<typeof setInterval>;
+
+		return {
+			start: async (_) => {
+				controller = _;
+
+				this.addController(user, controller);
+
+				on?.connect?.({ user, controller });
+
+				this.ping(controller);
+
+				ping = setInterval(() => {
+					try {
+						this.ping(controller);
+					} catch {
+						clearInterval(ping);
+
+						this.cancelController(user, controller, on?.disconnect);
+					}
+				}, 10 * 60000);
+			},
+			cancel: () => {
+				clearInterval(ping);
+
+				this.cancelController(user, controller, on?.disconnect);
+			},
+		};
+	};
+
+	public cancelController = (user: User, controller: Controller, onDisconnect?: OnAction) => {
+		try {
+			controller.close();
+		} catch {}
+
+		onDisconnect?.({ user, controller });
+
+		this.delController(user, controller);
+	};
+
+	private addController = (user: User, controller: Controller) => {
+		if (!this.users.has(user)) {
+			this.users.set(user, new Set());
+		}
+
+		return this.users.get(user)!.add(controller);
+	};
+
+	private getControllers = (user: User) => this.users.get(user);
+
+	private delController = (user: User, controller: Controller) =>
+		this.getControllers(user)?.delete(controller);
+
+	public send = (
+		userOrController: User | Controller,
+		id: MessageId | null,
+		channel: Channel,
+		data: MessageData = {}
+	) => {
+		if (typeof userOrController === "string" || typeof userOrController === "number") {
+			const controllers = this.getControllers(userOrController);
+
+			if (controllers) {
+				for (const controller of controllers) {
+					try {
+						controller.enqueue(this.message(id, channel, data));
+					} catch {}
+				}
+			}
+		} else {
+			try {
+				userOrController.enqueue(this.message(id, channel, data));
+			} catch {}
+		}
+	};
+
+	public sendEveryone = (channel: Channel, data?: MessageData) => {
+		for (const user of this.users.keys()) {
+			this.send(user, null, channel, data);
+		}
+	};
+
+	public ping = (controller: Controller) =>
+		controller.enqueue(this.message(null, "ping", Date.now()));
+
+	private message = (id: MessageId | null, channel: Channel, data: MessageData | number = {}) => {
+		`${id ? `id: ${id}\n` : ""}event: ${channel}\ndata: ${JSON.stringify(data)}\n\n`;
+	};
+}
--- src/types.ts
+++ src/types.ts
@@ -5,3 +5,18 @@ export interface Listener {
 	options?: boolean | AddEventListenerOptions;
 }
 
+export type Controller = ReadableStreamController<unknown>;
+
+export type ControllerId = number;
+
+export type Room = string | number;
+
+export type User = string | number;
+
+export type Channel = string | number;
+
+export type MessageId = string | number;
+
+export type MessageData = Record<string | number, any>;
+
+export type OnAction = (connection: { user: User; controller: Controller }) => any;
