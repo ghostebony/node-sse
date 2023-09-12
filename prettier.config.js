9cf14ff3e5fe5e916e6308c8668326acd8d92d02
Pedro Américo <pedroamericolouzada@uol.com.br>
2023-09-12 08:45:48 -0300

    update `devDependencies`

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Options} */
+export default {
+	useTabs: true,
+	singleQuote: false,
+	printWidth: 100,
+	tabWidth: 4,
+	arrowParens: "always",
+	bracketSpacing: true,
+	bracketSameLine: false,
+};
